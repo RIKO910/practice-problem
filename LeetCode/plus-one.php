b577acfa7eaeca35e3e644cc4d23049ff1c856b3
RIKO910 <tarikulislamriko910@gmail.com>
2024-09-18 21:12:25 +0600

    first commit

---
+++ LeetCode/plus-one.php
@@ -0,0 +1,29 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $digits
+     * @return Integer[]
+     */
+    function plusOne($digits) {
+        $n = count($digits);
+        $result = 0;
+        for($i =0 ; $i<$n ;$i++){
+            if($i != $n-1){
+                if($i == 0){
+                    $result = $digits[$i]*10;
+                }else{
+                    $result = ($result+$digits[$i])*10 ;
+                }
+
+            }else{
+                $result = $result+$digits[$n-1];
+            }
+
+        }
+        $explode = $result+1;
+        $arr  = array_map('intval', str_split($explode));
+        return $arr;
+    }
+}
