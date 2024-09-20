f28356d19fa324a3dd0222c08c4d04d2fda27315
RIKO910 <tarikulislamriko910@gmail.com>
2024-09-20 19:42:26 +0600

    search-insert-position

---
+++ LeetCode/search-insert-position.php
@@ -0,0 +1,16 @@
+<?php
+
+class Solution {
+    function searchInsert($nums, $target) {
+        $n = count($nums);
+
+        for ($i = 0; $i < $n; $i++) {
+            if ($nums[$i] == $target) {
+                return $i;
+            } else if ($nums[$i] > $target) {
+                return $i;
+            }
+        }
+        return $n;
+    }
+}
