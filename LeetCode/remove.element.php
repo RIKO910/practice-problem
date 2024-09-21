2215857027da034543024d18231de3c60b4a37d3
RIKO910 <tarikulislamriko910@gmail.com>
2024-09-21 19:00:26 +0600

    Remove Element

---
+++ LeetCode/remove.element.php
@@ -0,0 +1,21 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param  Integer[] $nums
+     * @param  Integer $val
+     * @return Integer
+     */
+    function removeElement(&$nums, $val) {
+        $n = count($nums);
+        for ($i = 0; $i < $n; $i++) {
+            if ($nums[$i] == $val) {
+                array_splice($nums, $i, 1);
+                $i--;
+                $n--;
+            }
+        }
+        return count($nums);
+    }
+}
