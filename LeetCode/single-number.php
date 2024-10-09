c4bd794b33ba221caa017210e60a34aa86fd69d9
RIKO910 <tarikulislamriko910@gmail.com>
2024-10-09 23:26:51 +0600

    single number

---
+++ LeetCode/single-number.php
@@ -0,0 +1,25 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function singleNumber($nums) {
+        $n = count($nums);
+        for($i = 0; $i < $n; $i++){
+            $isSingle = true;
+            for($j = 0; $j < $n; $j++){
+                if($i != $j && $nums[$i] == $nums[$j]){
+                    $isSingle = false;
+                    break;
+                }
+            }
+            if($isSingle) {
+                return $nums[$i];
+            }
+        }
+        return -1;
+    }
+}
