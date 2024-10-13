b553905fdde716faab771724cd3c5d8efce2ebb6
RIKO910 <tarikulislamriko910@gmail.com>
2024-10-13 23:49:30 +0600

    length of last word

---
+++ LeetCode/length-of-last-word.php
@@ -0,0 +1,20 @@
+<?php
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLastWord($s) {
+        // Trim any trailing and leading spaces
+        $s               = trim($s);
+        $last_word_start = strrpos($s, " ");
+
+        if ($last_word_start === false) {
+            return strlen($s);
+        }
+
+        $last_word = substr($s, $last_word_start + 1);
+        return strlen($last_word);
+    }
+}
