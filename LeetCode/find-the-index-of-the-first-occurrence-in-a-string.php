ce39542fd48bbc09f62d6350440aa74d85ef3049
RIKO910 <tarikulislamriko910@gmail.com>
2024-11-10 14:49:36 +0600

    find the index

---
+++ LeetCode/find-the-index-of-the-first-occurrence-in-a-string.php
@@ -0,0 +1,30 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $haystack
+     * @param String $needle
+     * @return Integer
+     */
+    function strStr($haystack, $needle) {
+        $n = strlen($haystack);
+        $m = strlen($needle);
+        
+        if ($m == 0) {
+            return 0; 
+        }
+        
+        if ($n < $m) {
+            return -1;
+        }
+        
+        for ($i = 0; $i <= $n - $m; $i++) {
+            if (substr($haystack, $i, $m) === $needle) {
+                return $i;
+            }
+        }
+        
+        return -1; 
+    }
+}
