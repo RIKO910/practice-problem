64dc745f09b0c6d00f543ae641a44b3028e5c6b9
RIKO910 <tarikulislamriko910@gmail.com>
2024-11-17 22:20:33 +0600

    Longest Common Prefix

---
+++ LeetCode/longest-common-prefix.php
@@ -0,0 +1,37 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs) {
+       $length = count($strs); 
+        $commonPrefix = ""; 
+
+        if ($length == 0) {
+            echo $commonPrefix;
+            return;
+        }
+
+        for ($k = 0; $k < strlen($strs[0]); $k++) { 
+            $isCommon = true; 
+
+            for ($i = 1; $i < $length; $i++) { 
+                if ($k >= strlen($strs[$i]) || $strs[0][$k] != $strs[$i][$k]) {
+                    $isCommon = false;
+                    break;
+                }
+            }
+
+            if ($isCommon) {
+                $commonPrefix .= $strs[0][$k]; 
+            } else {
+                break; 
+            }
+        }
+
+        return $commonPrefix; 
+    }
+}
