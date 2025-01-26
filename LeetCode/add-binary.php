1292990d7eb6d721744fea336c34181d315aee1c
RIKO910 <tarikulislamriko910@gmail.com>
2025-01-26 21:32:33 +0600

    add-binary

---
+++ LeetCode/add-binary.php
@@ -0,0 +1,30 @@
+<?php
+class Solution {
+
+    /**
+     * @param String $a
+     * @param String $b
+     * @return String
+     */
+    function addBinary($a, $b) {
+        $i = strlen($a) - 1;
+        $j = strlen($b) - 1;
+        $carry = 0;
+        $result = "";
+
+        while ($i >= 0 || $j >= 0 || $carry > 0) {
+            $bitA = $i >= 0 ? $a[$i] - '0' : 0;
+            $bitB = $j >= 0 ? $b[$j] - '0' : 0;
+
+            $sum = $bitA + $bitB + $carry;
+            $carry = $sum > 1 ? 1 : 0;
+            $result = ($sum % 2) . $result;
+
+            $i--;
+            $j--;
+        }
+
+        return $result;
+    }
+
+}
